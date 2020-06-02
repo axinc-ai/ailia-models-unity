3edd147a355a34950ebc019d29c889a7a9ccc699
ri shinpo <li@axinc.jp>
2020-06-02 22:39:50 +0900

    Make PoseEstimatorController inspector clearer.

--- Assets/AXIP/AILIA-MODELS/common/Editor/EstimatorsSampleInspector.cs
+++ Assets/AXIP/AILIA-MODELS/common/Editor/EstimatorsSampleInspector.cs
@@ -8,18 +8,24 @@ namespace ailiaSDK
 	[CustomEditor(typeof(AiliaPoseEstimatorsSample))]
 	public class EstimatorsSampleInspector : Editor
 	{
-		AiliaDetectorsSample ailiaDetectorsSample;
 		SerializedProperty ailiaModelType;
 		SerializedProperty uiCanvas;
+		SerializedProperty gpuMode;
+		SerializedProperty cameraID;
+		MonoScript script = null;
 
 		AiliaModelsConst.AiliaModelTypes[] modelArr;
 		string[] modelNameArr;
 		// Detectors category
 		const string category = "Pose Estimation";
-		private void OnEnable() {
+		private void OnEnable()
+		{
+			script = MonoScript.FromMonoBehaviour((AiliaPoseEstimatorsSample)target);
 
 			ailiaModelType = serializedObject.FindProperty("ailiaModelType");
 			uiCanvas = serializedObject.FindProperty("UICanvas");
+			gpuMode = serializedObject.FindProperty("gpu_mode");
+			cameraID = serializedObject.FindProperty("camera_id");
 			// Get all model types in the same category
 			// var category = ((AiliaModelsConst.AiliaModelTypes)ailiaModelType.enumValueIndex).GetCategory(); //Get category by ailiaModelType default value.
 			var allModelsTypes = Enum.GetValues(typeof(AiliaModelsConst.AiliaModelTypes)) as AiliaModelsConst.AiliaModelTypes[];
@@ -29,12 +35,17 @@ namespace ailiaSDK
 		public override void OnInspectorGUI()
 		{
 			serializedObject.Update();
-			base.OnInspectorGUI();
+			// base.OnInspectorGUI();
+			EditorGUI.BeginDisabledGroup(true);
+			EditorGUILayout.ObjectField("Script", script, typeof(MonoScript), false);
+			EditorGUI.EndDisabledGroup();
 
+			EditorGUILayout.PropertyField(uiCanvas);
+			EditorGUILayout.PropertyField(gpuMode);
+			EditorGUILayout.PropertyField(cameraID);
 			var currentIndex = Array.FindIndex(modelArr, x => x == (AiliaModelsConst.AiliaModelTypes)ailiaModelType.enumValueIndex);
 			currentIndex = EditorGUILayout.Popup("AiliaModelType", currentIndex, modelNameArr);
 			ailiaModelType.enumValueIndex = (int)modelArr[currentIndex];
-			EditorGUILayout.PropertyField(uiCanvas);
 
 			serializedObject.ApplyModifiedProperties();
 		}
