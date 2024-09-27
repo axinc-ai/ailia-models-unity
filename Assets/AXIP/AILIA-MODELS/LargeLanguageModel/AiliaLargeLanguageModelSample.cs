a3f27f2c0e457c99006dcd3e643756e5e281f9e2
Kazuki Kyakuno <kyakuno@axell.co.jp>
2024-09-27 15:15:39 +0900

    Added package

--- Assets/AXIP/AILIA-MODELS/LargeLanguageModel/AiliaLargeLanguageModelSample.cs
+++ Assets/AXIP/AILIA-MODELS/LargeLanguageModel/AiliaLargeLanguageModelSample.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+using ailia;
 using ailiaLLM;
 
 namespace ailiaSDK
@@ -23,7 +24,7 @@ namespace ailiaSDK
 		public InputField input_field;
 
 		// Settings
-		public LargeLanguageModelSampleModels gemma2_2b = LargeLanguageModelSampleModels.gemma2_2b;
+		public LargeLanguageModelSampleModels modelType = LargeLanguageModelSampleModels.gemma2_2b;
 		public bool gpu_mode = false;
 		public GameObject UICanvas = null;
 
@@ -32,17 +33,11 @@ namespace ailiaSDK
 		Text mode_text = null;
 
 		// AILIA
-		private AiliaLLMModel ailiaLLMModel = null;
+		private AiliaLLMModel llm = null;
     	private List<AiliaLLMChatMessage> messages = new List<AiliaLLMChatMessage>();
 
 		bool modelPrepared = false;
 		bool modelAllocated = false;
-
-		// Chunk
-		public TextAsset database = null;
-		private string[] chunk_text = null;
-		private List<float[]> chunk_embedding;
-		private int chunk_cnt = 0;
 
 		void Start()
 		{
@@ -68,27 +63,14 @@ namespace ailiaSDK
 		void AiliaInit()
 		{
 			// Create Ailia
-			ailiaModel = CreateAiliaNet(modelType, gpu_mode);
-
-			// Embedding
-			chunk_text = database.text.Split('\n');
-			chunk_embedding = new List<float[]>();
+			CreateAiliaNet(modelType, gpu_mode);
 		}
 
 
 		// Download models and Create ailiaModel
-		AiliaModel CreateAiliaNet(NaturalLanguageProcessingSampleModels modelType, bool gpu_mode = true)
+		void CreateAiliaNet(LargeLanguageModelSampleModels modelType, bool gpu_mode = true)
 		{
 			string asset_path = Application.temporaryCachePath;
-
-			ailiaModel = new AiliaModel();
-			if (gpu_mode)
-			{
-				// call before OpenFile
-				ailiaModel.Environment(Ailia.AILIA_ENVIRONMENT_TYPE_GPU);
-			}
-
-			ailiaTokenizer = new AiliaTokenizerModel();
 
 			AiliaDownload ailia_download = new AiliaDownload();
 			ailia_download.DownloaderProgressPanel = UICanvas.transform.Find("DownloaderProgressPanel").gameObject;
@@ -100,21 +82,17 @@ namespace ailiaSDK
 
 			StartCoroutine(ailia_download.DownloadWithProgressFromURL(urlList, () =>
 			{
+				llm.Create();
+				llm.Open(Application.streamingAssetsPath + "/gemma-2-2b-it-Q4_K_M.gguf");
 
-
-        llm.Create();
-        llm.Open(Application.streamingAssetsPath + "/gemma-2-2b-it-Q4_K_M.gguf");
-
-        AiliaLLMChatMessage message = new AiliaLLMChatMessage();
-        message.role = "system";
-        message.content = "語尾に「だわん」をつけてください。";
-        messages.Add(message);
+				AiliaLLMChatMessage message = new AiliaLLMChatMessage();
+				message.role = "system";
+				message.content = "語尾に「だわん」をつけてください。";
+				messages.Add(message);
 				if (modelPrepared == false){
 					Debug.Log("ailiaModel.OpenFile failed");
 				}
 			}));
-
-			return ailiaModel;
 		}
 
 		void Update()
@@ -152,7 +130,7 @@ namespace ailiaSDK
 
 		private void DestroyAilia()
 		{
-        llm.Close();
+        	llm.Close();
 		}
 
 		public void Submit(){
@@ -164,59 +142,30 @@ namespace ailiaSDK
 			string query_text = input_field.text;
 			string result = "";
 
+			AiliaLLMChatMessage message = new AiliaLLMChatMessage();
+			message.role = "user";
+			message.content = query_text;
+			messages.Add(message);
+			inputFiled.text = "";
 
-			        AiliaLLMChatMessage message = new AiliaLLMChatMessage();
-        message.role = "user";
-        message.content = inputFiled.text;
-        messages.Add(message);
-        inputFiled.text = "";
-
-        llm.SetPrompt(messages);
-        bool done = false;
-        string text = "";
-        while (true){
-            llm.Generate(ref done);
-            if (done == true){
-                break;
-            }
-            string deltaText = llm.GetDeltaText();
-            text = text + deltaText;
-            textField.text = text;
-        }
-        textField.text = text;
-
-        message = new AiliaLLMChatMessage();
-        message.role = "assistant";
-        message.content = text;
-        messages.Add(message);
-
-
-			if (modelType == NaturalLanguageProcessingSampleModels.fugumt_en_ja || modelType == NaturalLanguageProcessingSampleModels.fugumt_ja_en){
-				result = ailia_speech_translate.Translate(query_text);
-				label_text.text = result;
-			}
-
-			if (modelType == NaturalLanguageProcessingSampleModels.sentence_transformer_japanese || modelType == NaturalLanguageProcessingSampleModels.multilingual_e5){
-				long start_time = DateTime.UtcNow.Ticks / TimeSpan.TicksPerMillisecond;
-				if (chunk_cnt >= chunk_text.Length){
-					float [] query_embedding = textEmbedding.Embedding(query_text, ailiaModel, ailiaTokenizer);
-					float max_sim = 0.0f;
-					for (int i = 0; i < chunk_cnt; i++){
-						float sim = textEmbedding.CosSimilarity(query_embedding, chunk_embedding[i]);
-						Debug.Log(""+ chunk_text[i]+"/"+sim);
-						if (sim > max_sim){
-							max_sim = sim;
-							result = chunk_text[i];
-						}
-					}
-					result = "Query : "+query_text+"\nResult : "+result+" ("+max_sim+")\n";
+			llm.SetPrompt(messages);
+			bool done = false;
+			string text = "";
+			while (true){
+				llm.Generate(ref done);
+				if (done == true){
+					break;
 				}
-				long end_time = DateTime.UtcNow.Ticks / TimeSpan.TicksPerMillisecond;
-				if (label_text != null)
-				{
-					label_text.text = result+(end_time - start_time).ToString() + "ms\n" + ailiaModel.EnvironmentName();
-				}
+				string deltaText = llm.GetDeltaText();
+				text = text + deltaText;
+				label_text.text = text;
 			}
+			label_text.text = text;
+
+			message = new AiliaLLMChatMessage();
+			message.role = "assistant";
+			message.content = text;
+			messages.Add(message);
 		}
 	}
 }
